16d5bb2a448ad976e2027f385663dcf233103c9c
Julio Pontes <juliopfneto@gmail.com>
2012-11-20 21:18:05 -0300

    removed log

--- var/logs/command.2012-11-19.php
+++
@@ -1,8 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2012-11-19 20:52:22 UTC
-#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-Sep-2012 00:00 GMT
-
-#Fields: datetime	priority	category	message
-2012-11-19T20:52:22+00:00	INFO	command	help
-2012-11-19T20:52:55+00:00	INFO	command	help install
